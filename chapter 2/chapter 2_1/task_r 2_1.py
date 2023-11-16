aa9159fd27df9df7ee99cdd5455f15401e7361d0
paradox <paradox.tornio@gmail.com>
2023-11-16 16:39:10 +0200

    task r

--- chapter 2/chapter 2_1/task_r 2_1.py
+++ chapter 2/chapter 2_1/task_r 2_1.py
@@ -1,19 +1,3 @@
-binary_numbers = {
-    '0': 0,
-    '1': 1,
-    '10': 2,
-    '11': 3,
-    '100': 4,
-    '101': 5,
-    '110': 6,
-    '111': 7,
-    '1000': 8,
-    '1001': 9
-}
-n = input()
-n_list = list(n)
-price = 0
+n = int(input(), 2)
 m = int(input())
-for i in range(len(n_list)):
-    price += binary_numbers.get(n_list[i])
-print(price)
+print(m - n)
